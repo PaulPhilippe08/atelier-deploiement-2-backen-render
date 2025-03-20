96e71a74c139020a6c01e2f6f98011909677ac2b
DELL <paulphilippecoly@gmail.com>
2025-03-20 13:44:19 +0000

    ajout

--- backen/config/db.js
+++ backen/config/db.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://paulphilippe:1234567890@deploiementrender.pydzi.mongodb.net/?retryWrites=true&w=majority&appName=deploiementrender"
+    );
     console.log("✅ MongoDB connecté avec succès !");
   } catch (error) {
     console.error("❌ Erreur de connexion à MongoDB :", error);
